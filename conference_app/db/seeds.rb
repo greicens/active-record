a9ad41c329e2a4f1c884369e2d37af1c2ac3ac29
greicens <gsilva@mail.ccsf.edu>
2017-01-23 15:49:26 -0800

    adds data into seed file

--- conference_app/db/seeds.rb
+++ conference_app/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "Hello from seeds.rb"
+
+speakers_data = [
+    {:first=>"Ryan", :last=>"Thomas", :email=>"rt@example.com"},
+    {:first=>"Zach", :last=>"Cusimano", :email=>"zc@example.com"},
+    {:first=>"Ricardo", :last=>"Arellano", :email=>"ra@example.com"}
+]
+Speaker.delete_all
+Speaker.create(speakers_data)
